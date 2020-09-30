6b5ad7c80c9fe09c4692ab35d262ee511219dfc6
Youcef Bouterfass <cefyou28@gmail.com>
2020-09-30 09:05:20 +0200

    pyramide taille

--- pyramide.rb
+++ pyramide.rb
@@ -2,6 +2,12 @@ puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
 print "> "
 etage = gets.chomp.to_i
 
+while etage <= 1 || etage > 25
+    puts "La taille doit être comprise entre 1 et 25"
+    print "> "
+    etage = gets.chomp.to_i
+end
+
 puts "Voici la pyramide :"
 
 i = 1
